5d1bda791ff4aaaa30b9e45588bd7074f9535ec3
UrmatBolotbek <urmar9797@gmail.com>
2025-01-11 00:51:56 +0100

    обновил сервис конвертации валют

--- src/main/java/faang/school/paymentservice/config/api/CurrencyConfig.java
+++ src/main/java/faang/school/paymentservice/config/api/CurrencyConfig.java
@@ -1,18 +1,39 @@
 package faang.school.paymentservice.config.api;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import faang.school.paymentservice.config.properties.CurrencyApiProperties;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
-@Component
-@Setter
-@Getter
-@ConfigurationProperties(prefix = "currency")
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@RequiredArgsConstructor
 public class CurrencyConfig {
 
-    private String key;
-    private String base_url;
-    private String path;
+    private final CurrencyApiProperties properties;
 
+    @Bean
+    public WebClient currencyWebClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, properties.getTimeout().getConnection())
+                .responseTimeout(Duration.ofMillis(properties.getTimeout().getRead()))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(properties.getTimeout().getRead(),
+                                        TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(properties.getTimeout().getConnection(),
+                                        TimeUnit.MILLISECONDS)));
+
+        return WebClient.builder()
+                .baseUrl(properties.getUrl())
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
 }
---
+++ src/main/java/faang/school/paymentservice/model/enums/Currency.java
@@ -0,0 +1,16 @@
+package faang.school.paymentservice.model.enums;
+
+public enum Currency {
+    USD,
+    EUR,
+    RUB,
+    GBP,
+    CHF,
+    JPY,
+    CNY,
+    KGS,
+    KZT,
+    UZS,
+    TJS,
+    TMT
+}
